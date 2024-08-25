9d6826df031c99ef6c1c89d80ac1b376180b8f2c
Nima Saadati <nimodbdev@gmail.com>
2024-08-25 20:20:45 +0400

    Set up basic command-line interface for selecting conversion type

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Welcome to the Temperature Convertor!");
+    println!("Please select the Conversion type:");
+    println!("1: Fahrenheit to Celsius");
+    println!("2: Celsius to Fahrenheit");
+
+    let mut choice = String::new();
+    io::stdin()
+        .read_line(&mut choice)
+        .expect("Failed to read line.");
+
+    let choice: u32 = match choice.trim().parse() {
+        Ok(num) => num,
+        Err(_) => {
+            println!("Please enter a valid number");
+            return;
+        }
+    };
+
+    if choice == 1 {
+        println!("Conver Fahrenheit to Celsius");
+    } else if choice == 2 {
+        println!("Convert Celsius to Fahrenheit");
+    } else {
+        println!("Invalid choice, please select 1 or 2");
+    }
 }
