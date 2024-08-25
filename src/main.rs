9b6da2d63e0ec0bcf828206a310e1d38fe652922
Nima Saadati <nimodbdev@gmail.com>
2024-08-25 20:31:10 +0400

    Add Celsius and Fahrenheit conversion logic

--- src/main.rs
+++ src/main.rs
@@ -20,10 +20,52 @@ fn main() {
     };
 
     if choice == 1 {
-        println!("Conver Fahrenheit to Celsius");
+        println!("Enter temperature in Fahrenheit:");
+
+        let mut temp = String::new();
+        io::stdin()
+            .read_line(&mut temp)
+            .expect("Failed to read line");
+
+        let temp: f64 = match temp.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Please enter a valid number");
+                return;
+            }
+        };
+
+        let celsius = fahrenheit_to_celsius(temp);
+        println!("Converting Fahrenheit to Celsius...");
+        println!("{temp}°F is equal to {celsius:.2}°C");
     } else if choice == 2 {
-        println!("Convert Celsius to Fahrenheit");
+        println!("Enter temperature in Celsius:");
+
+        let mut temp = String::new();
+        io::stdin()
+            .read_line(&mut temp)
+            .expect("Failed to read line");
+
+        let temp: f64 = match temp.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Please enter a valid number");
+                return;
+            }
+        };
+
+        let fahrenheit = celsius_to_fahrenheit(temp);
+        println!("Converting Celsius to Fahrenheit...");
+        println!("{temp}°C is equal to {fahrenheit:.2}°F");
     } else {
         println!("Invalid choice, please select 1 or 2");
     }
+}
+
+fn fahrenheit_to_celsius(f: f64) -> f64 {
+    (f - 32.0) * 5.0 / 9.0
+}
+
+fn celsius_to_fahrenheit(c: f64) -> f64 {
+    (c * 9.0 / 5.0) + 32.0
 }
